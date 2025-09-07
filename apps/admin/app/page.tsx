4dee88ed29b65ddc7a0204e4ba8bf8597cd6e6c6
Juddan <juddan2008@gmail.com>
2025-09-07 10:32:42 +0530

    first commit

---
+++ apps/admin/app/page.tsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import { Button } from '@workspace/ui/components/button';
+
+export default function Page() {
+  return (
+    <div className='flex items-center justify-center min-h-svh'>
+      <div className='flex flex-col items-center justify-center gap-4'>
+        <h1 className='text-2xl font-bold'>Hello admin</h1>
+        <Button size='sm'>Button</Button>
+      </div>
+    </div>
+  );
+}
