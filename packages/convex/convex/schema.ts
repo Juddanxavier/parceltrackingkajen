6a4bcadd0d1d211d846078e966e01debedfd7073
Juddan <juddan2008@gmail.com>
2025-09-07 18:20:29 +0530

    convex connected

---
+++ packages/convex/convex/schema.ts
@@ -0,0 +1,12 @@
+/** @format */
+
+import { defineSchema, defineTable } from 'convex/server';
+import { v } from 'convex/values';
+
+export default defineSchema({
+  users: defineTable({
+    name: v.string(),
+    email: v.string(),
+    password: v.string(),
+  }),
+});
